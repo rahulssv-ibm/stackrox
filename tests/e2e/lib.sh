ccronca/stackrox_stackrox

roxctl passwords

tests/e2e/lib.sh
@@ -235,8 +235,8 @@ deploy_central_via_operator() {
 
     NAMESPACE="${central_namespace}" make -C operator stackrox-image-pull-secret
 
-    ROX_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
-    centralAdminPasswordBase64="$(echo "$ROX_PASSWORD" | base64)"
+    ROX_ADMIN_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
+    centralAdminPasswordBase64="$(echo "$ROX_ADMIN_PASSWORD" | base64)"
 
     centralAdditionalCAIndented="$(sed 's,^,        ,' "${TRUSTED_CA_FILE:-/dev/null}")"
     if [[ -z $centralAdditionalCAIndented ]]; then
@@ -373,7 +373,6 @@ deploy_sensor_via_operator() {
     kubectl -n "${central_namespace}" exec deploy/central -- \
     roxctl central init-bundles generate my-test-bundle \
         --insecure-skip-tls-verify \
-        --password "$ROX_PASSWORD" \
         --output-secrets - \
     | kubectl -n "${sensor_namespace}" apply -f -
 
@@ -441,17 +440,17 @@ pause_stackrox_operator_reconcile() {
 export_central_basic_auth_creds() {
     if [[ -n ${DEPLOY_DIR:-} && -f "${DEPLOY_DIR}/central-deploy/password" ]]; then
         info "Getting central basic auth creds from central-deploy/password"
-        ROX_PASSWORD="$(cat "${DEPLOY_DIR}"/central-deploy/password)"
-    elif [[ -n "${ROX_PASSWORD:-}" ]]; then
-        info "Using existing ROX_PASSWORD env"
+        ROX_ADMIN_PASSWORD="$(cat "${DEPLOY_DIR}"/central-deploy/password)"
+    elif [[ -n "${ROX_ADMIN_PASSWORD:-}" ]]; then
+        info "Using existing ROX_ADMIN_PASSWORD env"
     else
-        echo "Expected to find file ${DEPLOY_DIR}/central-deploy/password or ROX_PASSWORD env"
+        echo "Expected to find file ${DEPLOY_DIR}/central-deploy/password or ROX_ADMIN_PASSWORD env"
         exit 1
     fi
 
     ROX_USERNAME="admin"
     ci_export "ROX_USERNAME" "$ROX_USERNAME"
-    ci_export "ROX_PASSWORD" "$ROX_PASSWORD"
+    ci_export "ROX_ADMIN_PASSWORD" "$ROX_ADMIN_PASSWORD"
 }
 
 deploy_optional_e2e_components() {
@@ -488,10 +487,10 @@ setup_client_CA_auth_provider() {
     info "Set up client CA auth provider for endpoints_test.go"
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
     require_environment "CLIENT_CA_PATH"
 
-    roxctl -e "$API_ENDPOINT" -p "$ROX_PASSWORD" \
+    roxctl -e "$API_ENDPOINT" \
         central userpki create test-userpki -r Analyst -c "$CLIENT_CA_PATH"
 }
 
@@ -505,9 +504,9 @@ setup_generated_certs_for_test() {
     local dir="$1"
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
-    roxctl -e "$API_ENDPOINT" -p "$ROX_PASSWORD" \
+    roxctl -e "$API_ENDPOINT" \
         sensor generate-certs remote --output-dir "$dir"
     [[ -f "$dir"/cluster-remote-tls.yaml ]]
     # Use the certs in future steps that will use client auth.
@@ -1106,12 +1105,12 @@ _record_build_info() {
 
     local central_namespace=${1:-stackrox}
 
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
     local build_info
 
     local metadata_url="https://${API_ENDPOINT}/v1/metadata"
-    releaseBuild="$(curl -skS --config <(escape user "admin:${ROX_PASSWORD}") "${metadata_url}" | jq -r '.releaseBuild')"
+    releaseBuild="$(curl -skS --config <(escape user "admin:${ROX_ADMIN_PASSWORD}") "${metadata_url}" | jq -r '.releaseBuild')"
 
     if [[ "$releaseBuild" == "true" ]]; then
         build_info="release"
@@ -1134,10 +1133,10 @@ restore_4_1_postgres_backup() {
     info "Restoring a 4.1 postgres backup"
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
     gsutil cp gs://stackrox-ci-upgrade-test-fixtures/upgrade-test-dbs/postgres_db_4_1.sql.zip .
-    roxctl -e "$API_ENDPOINT" -p "$ROX_PASSWORD" \
+    roxctl -e "$API_ENDPOINT" \
         central db restore --timeout 5m postgres_db_4_1.sql.zip
 }
 
@@ -1160,21 +1159,21 @@ db_backup_and_restore_test() {
     fi
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
     # Ensure central is ready for requests after any previous tests
     wait_for_api "${central_namespace}"
 
     info "Backing up to ${output_dir}"
     mkdir -p "$output_dir"
-    roxctl -e "${API_ENDPOINT}" -p "${ROX_PASSWORD}" central backup --output "$output_dir" || touch DB_TEST_FAIL
+    roxctl -e "${API_ENDPOINT}" central backup --output "$output_dir" || touch DB_TEST_FAIL
 
     info "Updating public config"
     update_public_config
 
     if [[ ! -e DB_TEST_FAIL ]]; then
         info "Restoring from ${output_dir}/postgres_db_*"
-        roxctl -e "${API_ENDPOINT}" -p "${ROX_PASSWORD}" central db restore "$output_dir"/postgres_db_* || touch DB_TEST_FAIL
+        roxctl -e "${API_ENDPOINT}" central db restore "$output_dir"/postgres_db_* || touch DB_TEST_FAIL
     fi
 
     wait_for_api "${central_namespace}"

tests/roxctl/token-file.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-# This test script requires API_ENDPOINT and ROX_PASSWORD to be set in the environment.
+# This test script requires API_ENDPOINT and ROX_ADMIN_PASSWORD to be set in the environment.
 
 [ -n "$API_ENDPOINT" ]
-[ -n "$ROX_PASSWORD" ]
+[ -n "$ROX_ADMIN_PASSWORD" ]
 
 echo "Using API_ENDPOINT $API_ENDPOINT"
 
@@ -20,7 +20,7 @@ escape() {
 # Retrieve API token
 TOKEN_FILE=$(mktemp)
 curl -k -f \
-  --config <(escape user "admin:$ROX_PASSWORD") \
+  --config <(escape user "admin:$ROX_ADMIN_PASSWORD") \
   -d '{"name": "test", "role": "Admin"}' \
   --retry 5 \
   --retry-connrefused \
@@ -80,7 +80,6 @@ test_roxctl_cmd() {
 
   # Verify that a password on the command line has precedence over token in the environment
   if OUTPUT=$(ROX_API_TOKEN="invalid-token" roxctl --insecure-skip-tls-verify --insecure -e "$API_ENDPOINT" \
-    --password "$ROX_PASSWORD" \
     "$@" \
     2>&1); then
       echo "[OK] --password has precedence over ROX_API_TOKEN environment variable"

rahulssv-ibm/stackrox

tests/e2e/lib.sh
@@ -238,8 +238,8 @@ deploy_central_via_operator() {
 
     NAMESPACE="${central_namespace}" make -C operator stackrox-image-pull-secret
 
-    ROX_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
-    centralAdminPasswordBase64="$(echo "$ROX_PASSWORD" | base64)"
+    ROX_ADMIN_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
+    centralAdminPasswordBase64="$(echo "$ROX_ADMIN_PASSWORD" | base64)"
 
     centralAdditionalCAIndented="$(sed 's,^,        ,' "${TRUSTED_CA_FILE:-/dev/null}")"
     if [[ -z $centralAdditionalCAIndented ]]; then
@@ -382,7 +382,6 @@ deploy_sensor_via_operator() {
     kubectl -n "${central_namespace}" exec deploy/central -- \
     roxctl central init-bundles generate my-test-bundle \
         --insecure-skip-tls-verify \
-        --password "$ROX_PASSWORD" \
         --output-secrets - \
     | kubectl -n "${sensor_namespace}" apply -f -
 
@@ -450,17 +449,17 @@ pause_stackrox_operator_reconcile() {
 export_central_basic_auth_creds() {
     if [[ -n ${DEPLOY_DIR:-} && -f "${DEPLOY_DIR}/central-deploy/password" ]]; then
         info "Getting central basic auth creds from central-deploy/password"
-        ROX_PASSWORD="$(cat "${DEPLOY_DIR}"/central-deploy/password)"
-    elif [[ -n "${ROX_PASSWORD:-}" ]]; then
-        info "Using existing ROX_PASSWORD env"
+        ROX_ADMIN_PASSWORD="$(cat "${DEPLOY_DIR}"/central-deploy/password)"
+    elif [[ -n "${ROX_ADMIN_PASSWORD:-}" ]]; then
+        info "Using existing ROX_ADMIN_PASSWORD env"
     else
-        echo "Expected to find file ${DEPLOY_DIR}/central-deploy/password or ROX_PASSWORD env"
+        echo "Expected to find file ${DEPLOY_DIR}/central-deploy/password or ROX_ADMIN_PASSWORD env"
         exit 1
     fi
 
     ROX_USERNAME="admin"
     ci_export "ROX_USERNAME" "$ROX_USERNAME"
-    ci_export "ROX_PASSWORD" "$ROX_PASSWORD"
+    ci_export "ROX_ADMIN_PASSWORD" "$ROX_ADMIN_PASSWORD"
 }
 
 deploy_optional_e2e_components() {
@@ -497,10 +496,10 @@ setup_client_CA_auth_provider() {
     info "Set up client CA auth provider for endpoints_test.go"
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
     require_environment "CLIENT_CA_PATH"
 
-    roxctl -e "$API_ENDPOINT" -p "$ROX_PASSWORD" \
+    roxctl -e "$API_ENDPOINT" \
         central userpki create test-userpki -r Analyst -c "$CLIENT_CA_PATH"
 }
 
@@ -514,9 +513,9 @@ setup_generated_certs_for_test() {
     local dir="$1"
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
-    roxctl -e "$API_ENDPOINT" -p "$ROX_PASSWORD" \
+    roxctl -e "$API_ENDPOINT" \
         sensor generate-certs remote --output-dir "$dir"
     [[ -f "$dir"/cluster-remote-tls.yaml ]]
     # Use the certs in future steps that will use client auth.
@@ -1133,12 +1132,12 @@ _record_build_info() {
 
     local central_namespace=${1:-stackrox}
 
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
     local build_info
 
     local metadata_url="https://${API_ENDPOINT}/v1/metadata"
-    releaseBuild="$(curl -skS --config <(curl_cfg user "admin:${ROX_PASSWORD}") "${metadata_url}" | jq -r '.releaseBuild')"
+    releaseBuild="$(curl -skS --config <(curl_cfg user "admin:${ROX_ADMIN_PASSWORD}") "${metadata_url}" | jq -r '.releaseBuild')"
 
     if [[ "$releaseBuild" == "true" ]]; then
         build_info="release"
@@ -1161,10 +1160,10 @@ restore_4_1_postgres_backup() {
     info "Restoring a 4.1 postgres backup"
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
     gsutil cp gs://stackrox-ci-upgrade-test-fixtures/upgrade-test-dbs/postgres_db_4_1.sql.zip .
-    roxctl -e "$API_ENDPOINT" -p "$ROX_PASSWORD" \
+    roxctl -e "$API_ENDPOINT" \
         central db restore --timeout 5m postgres_db_4_1.sql.zip
 }
 
@@ -1187,21 +1186,21 @@ db_backup_and_restore_test() {
     fi
 
     require_environment "API_ENDPOINT"
-    require_environment "ROX_PASSWORD"
+    require_environment "ROX_ADMIN_PASSWORD"
 
     # Ensure central is ready for requests after any previous tests
     wait_for_api "${central_namespace}"
 
     info "Backing up to ${output_dir}"
     mkdir -p "$output_dir"
-    roxctl -e "${API_ENDPOINT}" -p "${ROX_PASSWORD}" central backup --output "$output_dir" || touch DB_TEST_FAIL
+    roxctl -e "${API_ENDPOINT}" central backup --output "$output_dir" || touch DB_TEST_FAIL
 
     info "Updating public config"
     update_public_config
 
     if [[ ! -e DB_TEST_FAIL ]]; then
         info "Restoring from ${output_dir}/postgres_db_*"
-        roxctl -e "${API_ENDPOINT}" -p "${ROX_PASSWORD}" central db restore "$output_dir"/postgres_db_* || touch DB_TEST_FAIL
+        roxctl -e "${API_ENDPOINT}" central db restore "$output_dir"/postgres_db_* || touch DB_TEST_FAIL
     fi
 
     wait_for_api "${central_namespace}"

tests/roxctl/token-file.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-# This test script requires API_ENDPOINT and ROX_PASSWORD to be set in the environment.
+# This test script requires API_ENDPOINT and ROX_ADMIN_PASSWORD to be set in the environment.
 
 [ -n "$API_ENDPOINT" ]
-[ -n "$ROX_PASSWORD" ]
+[ -n "$ROX_ADMIN_PASSWORD" ]
 
 echo "Using API_ENDPOINT $API_ENDPOINT"
 
@@ -20,7 +20,7 @@ curl_cfg() { # Use built-in echo to not expose $2 in the process list.
 # Retrieve API token
 TOKEN_FILE=$(mktemp)
 curl -k -f \
-  --config <(curl_cfg user "admin:$ROX_PASSWORD") \
+  --config <(curl_cfg user "admin:$ROX_ADMIN_PASSWORD") \
   -d '{"name": "test", "role": "Admin"}' \
   --retry 5 \
   --retry-connrefused \
@@ -80,7 +80,6 @@ test_roxctl_cmd() {
 
   # Verify that a password on the command line has precedence over token in the environment
   if OUTPUT=$(ROX_API_TOKEN="invalid-token" roxctl --insecure-skip-tls-verify --insecure -e "$API_ENDPOINT" \
-    --password "$ROX_PASSWORD" \
     "$@" \
     2>&1); then
       echo "[OK] --password has precedence over ROX_API_TOKEN environment variable"